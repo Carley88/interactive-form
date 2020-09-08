50e6e21102e42a62e759fce9a5c8989c4335054c
Carley88 <carleyjeffrey88@gmail.com>
2020-09-08 12:28:40 +0100

    Added a change event listener to the title (job role) element. Put an if statement in place to remove the is-hidden class when the other job role is selected

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ const name = document.getElementById("name");
 name.focus();
 
 const otherTitle = document.getElementById("other-title");
-otherTitle.className = "is-hidden"
+otherTitle.className = "is-hidden";
+
+const jobRole = document.getElementById("title");
+jobRole.addEventListener('change', (event) => {
+  if(event.target.value === "other") {
+    otherTitle.className = ""
+  } else {
+    otherTitle.className = 'is-hidden'
+  }
+})
