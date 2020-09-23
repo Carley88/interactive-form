f5c523c14def4c78c9439e918f5e89d2a4a58418
Carley Jeffrey <carleyjeffrey88@gmail.com>
2020-09-23 17:05:30 +0100

    Add if statement to cc validation: validation will only run if the user has selected the credit card option

--- js/script.js
+++ js/script.js
@@ -151,21 +151,25 @@ function validateActivities() {
 function validateCardDetails() {
   const cardNumber = document.getElementById("cc-num");
   const cardNumberTest = /^\d{13,16}$/.test(cardNumber.value);
-  if (cardNumberTest === false) {
-    console.log("Card number is incomplete, please enter a valid card number");
-  }
-  const zip = document.getElementById("zip");
-  const zipTest = /^\d{5}$/.test(zip.value);
-  if (zipTest === false) {
-    console.log("Zip is incomplete, please enter a valid zip");
-  }
-  const cvv = document.getElementById("cvv");
-  const cvvTest = /^\d{3}$/.test(cvv.value);
-  if (cvvTest === false) {
-    console.log("CVV number incomplete, please enter a valid CVV");
-  }
-  if (cardNumberTest === false || zipTest === false || cvvTest === false) {
-    return(false);
+  if (creditCard.style.display === "") {
+    if (cardNumberTest === false) {
+      console.log("Card number is incomplete, please enter a valid card number");
+    }
+    const zip = document.getElementById("zip");
+    const zipTest = /^\d{5}$/.test(zip.value);
+    if (zipTest === false) {
+      console.log("Zip is incomplete, please enter a valid zip");
+    }
+    const cvv = document.getElementById("cvv");
+    const cvvTest = /^\d{3}$/.test(cvv.value);
+    if (cvvTest === false) {
+      console.log("CVV number incomplete, please enter a valid CVV");
+    }
+    if (cardNumberTest === false || zipTest === false || cvvTest === false) {
+      return(false);
+    }
+  } else {
+    return ("Card not selected")
   }
 }
 
@@ -174,5 +178,4 @@ submitButton = document.getElementsByTagName("button")[0];
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
   validateCardDetails();
-
 })
